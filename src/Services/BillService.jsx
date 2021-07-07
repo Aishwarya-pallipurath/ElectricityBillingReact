e3d38752dc4b16e5e868f32136c1df90247af0ce
Aishwarya-pallipurath <76932757+Aishwarya-pallipurath@users.noreply.github.com>
2021-07-07 14:12:54 +0530

    Update BillService.jsx

--- src/Services/BillService.jsx
+++ src/Services/BillService.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const CONSUMER_API_BASE_URL = "http://ec2-54-91-121-58.compute-1.amazonaws.com:8081";
+const CONSUMER_API_BASE_URL = "http://ec2-54-163-61-125.compute-1.amazonaws.com:8081";
 
 class BillService {
 
