dfe53326e7ad1e5094f0979d7e5e3bf28db23903
Aishwarya-pallipurath <76932757+Aishwarya-pallipurath@users.noreply.github.com>
2021-07-07 14:14:55 +0530

    Update CustomerService.jsx

--- src/Services/CustomerService.jsx
+++ src/Services/CustomerService.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import axios from 'axios'
-const customer_base_url="http://ec2-54-91-121-58.compute-1.amazonaws.com:8081/api/v1/customers"
-const LOGIN_API_BASE_URL="http://ec2-54-91-121-58.compute-1.amazonaws.com:8081/api/v1";
+const customer_base_url="http://ec2-54-163-61-125.compute-1.amazonaws.com:8081/api/v1/customers"
+const LOGIN_API_BASE_URL="http://ec2-54-163-61-125.compute-1.amazonaws.com:8081/api/v1";
 class CustomerService extends React.Component{
     getAllCustomers(){
         axios.get(customer_base_url);
