7b117976e8b02e5a78d16a55d52fad0b0394f4c6
Aishwarya-pallipurath <76932757+Aishwarya-pallipurath@users.noreply.github.com>
2021-07-07 15:04:59 +0530

    Update BillActions.js

--- src/actions/BillActions.js
+++ src/actions/BillActions.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
-import { GET_ERRORS,GET_BILLS,GET_BILL,DELETE_BILL } from './type';
+import { GET_ERRORS,GET_BILLS,GET_BILL,DELETE_BILL } from './type'; 
+import BillService from '../Services/BillService';
+
 export const createBill=(bill,history)=>async dispatch=> {
     try {
-        const res =await axios.post ("http://localhost:8081/createNewBill",bill)
+        const res =await BillService.createBill(Bill);
         history.push("/BillDashboard");
     } catch (error) {
         dispatch({
@@ -13,7 +15,7 @@ export const createBill=(bill,history)=>async dispatch=> {
 }
 
 export const getBills=()=>async dispatch=>{
-    const res=await axios.get("http://localhost:8081/getAllBills");
+    const res=await BillService.getAllBills();
       dispatch({
         type:GET_BILLS,
         payload:res.data
@@ -21,7 +23,7 @@ export const getBills=()=>async dispatch=>{
 }
 
 export const getBill=(billId,history)=>async dispatch=>{
-    const res=await axios.get(`http://localhost:8081/getBillById/${billId}`);
+    const res=await BillService.getBillById(billId);
       dispatch({
         type:GET_BILL,
         payload:res.data
@@ -30,7 +32,7 @@ export const getBill=(billId,history)=>async dispatch=>{
 
 export const deleteBill=(billId)=>async dispatch=>{
     if(window.confirm("Are you sure ? This will delete the bill and the data related to it")) {
-        await axios.delete(`http://localhost:8081/deleteBillById/${billId}`);
+        await BillService.deleteBillById(billId)
         dispatch({
             type:DELETE_BILL,
             payload:billId
